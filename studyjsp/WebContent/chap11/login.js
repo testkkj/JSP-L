9024e2906e71d06ac48a8d1c92d3685f2f9c4fcf
Administrator <Administrator@G111>
2019-05-28 16:42:08 +0900

    dda ger

--- studyjsp/WebContent/chap11/login.js
+++ studyjsp/WebContent/chap11/login.js
@@ -4,50 +4,51 @@ $(document).ready(function() {
 	$("#register").click(function() {
 		$("main_auth").load("registerForm.jsp");
 	});
-});
+	
+	$("#login").click(function() {
+		checkIt();
+		if (status) {
+			var query = {
+				id : $("#id").val(),
+				passwd : $("#passwd").val()
+			};
 
-$("#login").click(function() {
-	checkIt();
-	if (status) {
-		var query = {
-			id : $("#id").val(),
-			passwd : $("#passwd").val()
-		};
-
-		$.ajaz({
-			type : "POST",
-			url : "loginPro.jsp",
-			data : query,
-			success : function(data) {
-				if (data == 1) {
-					$("#main_auth").load("loginForm.jsp");
-				} else if (data == 0) {
-					alert("비밀번호가 맞지 않습니다.");
-					$("#id").val("");
-					$("#passwd").focus();
-				} else if (data == -1) {
-					alert("아이디가 맞지 않습니다.");
-					$("#id").val("");
-					$("#passwd").val("");
-					$("#id").focus();
+			$.ajax({
+				type : "POST",
+				url : "loginPro.jsp",
+				data : query,
+				success : function(data) {
+					if (data == 1) {
+						$("#main_auth").load("loginForm.jsp");
+					} else if (data == 0) {
+						alert("비밀번호가 맞지 않습니다.");
+						$("#id").val("");
+						$("#passwd").focus();
+					} else if (data == -1) {
+						alert("아이디가 맞지 않습니다.");
+						$("#id").val("");
+						$("#passwd").val("");
+						$("#id").focus();
+					}
 				}
-			}
-		});
-	}
-});
-
-$("#update").click(function() {
-	$("#main_auth").load("modify.jsp");
-});
-
-$("#logout").click(function() {
-	$.ajax({
-		type : "POST",
-		url : "logout.jsp",
-		success : function(data) {
-			$("#main_auth").load("loginForm.jsp");
+			});
 		}
 	});
+	
+	$("#update").click(function() {
+		$("#main_auth").load("modify.jsp");
+	});
+
+	$("#logout").click(function() {
+		$.ajax({
+			type : "POST",
+			url : "logout.jsp",
+			success : function(data) {
+				$("#main_auth").load("main.jsp");
+			}
+		});
+	});
+	
 });
 
 function checkIt() {
