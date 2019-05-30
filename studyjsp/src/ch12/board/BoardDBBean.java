de7df4a840b0eb130357e33b8fcf080e464b8e80
Administrator <Administrator@G111>
2019-05-30 16:37:54 +0900

    chapter12 working

---
+++ studyjsp/src/ch12/board/BoardDBBean.java
@@ -0,0 +1,332 @@
+package ch12.board;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BoardDBBean {
+
+	private static BoardDBBean instance = new BoardDBBean();
+
+	public static BoardDBBean getInstance() {
+		return instance;
+	}
+
+	private BoardDBBean() {
+
+	}
+
+	private Connection getConnection() throws Exception {
+		Context initCtx = new InitialContext();
+		Context envCtx = (Context) initCtx.lookup("java:comp/env");
+		DataSource ds = (DataSource) envCtx.lookup("jdbc/jsptest");
+		return ds.getConnection();
+	}
+
+	public int insertArticle(BoardDataBean article) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int x = 0;
+		int number = 0;
+		String sql = "";
+
+		int num = article.getNum();
+		int ref = article.getRef();
+		int re_step = article.getRe_step();
+		int re_level = article.getRe_level();
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select max(num) from board");
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				number = rs.getInt(1) + 1;
+			} else {
+				number = 1;
+			}
+
+			if (num != 0) {
+				sql = "update board set re_step=re_step+1 where ref=? and re_step>?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, ref);
+				pstmt.setInt(2, re_step);
+				pstmt.executeUpdate();
+				re_step = re_step + 1;
+				re_level = re_level + 1;
+			} else {
+				ref = number;
+				re_step = 0;
+				re_level = 0;
+			}
+
+			sql = "insert into board(writer, subject, content, passwd, reg_date,ip, ref, re_step, re_level) values(?.?.?.?.?.?.?.?.?)";
+			pstmt.setString(1, article.getWriter());
+			pstmt.setString(2, article.getSubject());
+			pstmt.setString(3, article.getContent());
+			pstmt.setString(4, article.getPasswd());
+			pstmt.setTimestamp(5, article.getReg_date());
+			pstmt.setString(6, article.getIp());
+			pstmt.setInt(7, ref);
+			pstmt.setInt(8, re_step);
+			pstmt.setInt(9, re_level);
+			pstmt.executeUpdate();
+			x = 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+
+		return x;
+	}
+
+	public int getArticleCount() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int x = 0;
+
+		try {
+			conn = getConnection();
+
+			pstmt = conn.prepareStatement("select count(*) from board");
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				x = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+
+		return x;
+	}
+
+	public List<BoardDataBean> getArticles(int start, int end) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardDataBean> articleList = null;
+		try {
+			conn = getConnection();
+
+			pstmt = conn.prepareStatement("select * from board order by ref desc, re_step asc limit ?,?");
+			pstmt.setInt(1, start - 1);
+			pstmt.setInt(2, end);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				articleList = new ArrayList<BoardDataBean>(end);
+				do {
+					BoardDataBean article = new BoardDataBean();
+					article.setNum(rs.getInt("num"));
+					article.setWriter(rs.getString("writer"));
+					article.setSubject(rs.getString("subject"));
+					article.setContent(rs.getString("content"));
+					article.setPasswd(rs.getString("passwd"));
+					article.setReg_date(rs.getTimestamp("reg_date"));
+					article.setReadcount(rs.getInt("readcount"));
+					article.setRef(rs.getInt("ref"));
+					article.setRe_step(rs.getInt("re_step"));
+					article.setRe_level(rs.getInt("re_level"));
+					article.setContent(rs.getString("content"));
+					article.setIp(rs.getString("ip"));
+					articleList.add(article);
+				} while (rs.next());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+		return articleList;
+	}
+
+	public BoardDataBean updateGetArticle(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardDataBean> articleList = null;
+		BoardDataBean article = null;
+		try {
+			conn = getConnection();
+
+			pstmt = conn.prepareStatement("select * from board where num=?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				article = new BoardDataBean();
+				article.setNum(rs.getInt("num"));
+				article.setWriter(rs.getString("writer"));
+				article.setSubject(rs.getString("subject"));
+				article.setContent(rs.getString("content"));
+				article.setPasswd(rs.getString("passwd"));
+				article.setReg_date(rs.getTimestamp("reg_date"));
+				article.setReadcount(rs.getInt("readcount"));
+				article.setRef(rs.getInt("ref"));
+				article.setRe_step(rs.getInt("re_step"));
+				article.setRe_level(rs.getInt("re_level"));
+				article.setContent(rs.getString("content"));
+				article.setIp(rs.getString("ip"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+		return article;
+	}
+
+	public int updateArticle(BoardDataBean article) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int x = -1;
+
+		try {
+			conn = getConnection();
+
+			pstmt = conn.prepareStatement("select passwd from board where num=?");
+			pstmt.setInt(1, article.getNum());
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				String dbpasswd = rs.getString("passwd");
+				if (dbpasswd.equals(article.getPasswd())) {
+					String sql = "update board set subject=?, content=? where num=?";
+					pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, article.getSubject());
+					pstmt.setString(2, article.getContent());
+					pstmt.setInt(3, article.getNum());
+					pstmt.executeUpdate();
+					x = 1;
+				} else {
+					x = 0;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+
+		return x;
+	}
+
+	public int deleteArticle(int num, String passwd) {
+		p=488;
+	}
+
+}
